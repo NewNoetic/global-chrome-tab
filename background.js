140bd5a05f37ec14dd2952c56bc981493c170e39
sdgandhi <sdgandhi@users.noreply.github.com>
2020-04-22 19:06:01 -0700

    Fix new tab focus when Chrome apps windows are open

--- background.js
+++ background.js
@@ -1,8 +1,18 @@
 chrome.commands.onCommand.addListener(function(command) {
   if (command === 'new-tab') {
-    chrome.windows.getCurrent({}, function(win){
-      chrome.windows.update(win.id, {focused: true}, function() {
-        chrome.tabs.create({});
+    chrome.windows.getAll({populate: true}, function(wins) {
+      let sortedByTabs = wins.slice().sort(function(a, b) {
+        if (a.tabs.length > b.tabs.length) {
+          return 1
+        } else if (a.tabs.length < b.tabs.length) {
+          return -1
+        } else {
+          return 0
+        }
+      })
+      let targetWindow = sortedByTabs.shift()
+      chrome.tabs.create({windowId: targetWindow.id}, function(tab) {
+        chrome.windows.update(tab.windowId, {focused: true});
       });
     });
   } else if (command === 'new-window') {
